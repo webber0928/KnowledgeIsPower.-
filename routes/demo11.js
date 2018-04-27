dc253e7d452129f678cd301a6702c71b9215d537
webber <bird23074035@gmail.com>
2018-04-27 14:16:54 +0800

    localhost demo

--- routes/demo11.js
+++ routes/demo11.js
@@ -1,9 +1,35 @@
 "use strict";
 
-console.log('----This Demo11');
+console.log('----Demo11 Reboot Device');
 
-const { dialogflow } = require("actions-on-google");
-const app = dialogflow({ debug: true });
+const { dialogflow, Suggestions } = require("actions-on-google");
+const app = dialogflow({ debug: false });
 
+// if I have two device
+const myDevice = [
+  'rdsg-device1',
+  'rdsg-device2'
+];
+//-----------------
+
+app.intent('RebootDevice', (conv, params) => {
+  if(myDevice.indexOf(params.DeviceName) >= 0){
+    return conv.ask(`rdsg OK, are you sure you want to reboot ${params.DeviceName}?`);
+  }
+  conv.ask(`Which one? ${myDevice[0]}? ${myDevice[1]}?`);
+  conv.ask(new Suggestions([
+    `restart ${myDevice[0]}`,
+    `restart ${myDevice[1]}`
+  ]));
+});
+
+app.intent('RebootDevice - yes', (conv) => {
+  let deviceName = conv.contexts.input['rebootdevice-followup'].parameters.DeviceName;
+  conv.close(`OK! I see! reboot ${deviceName}`);
+});
+
+app.intent('RebootDevice - no', (conv) => {
+  conv.close(`OK!`);
+});
 
 module.exports = app;
